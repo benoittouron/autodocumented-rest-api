52b04ec83fd9c224980c3f9b0ade8090bb6c3726
Ben <benoit.touron@atos.net>
2019-03-21 19:11:29 +0100

    First commit

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+    @Id private String id;
+
+    private String firstName;
+    private String lastName;
+    private String avatar;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
